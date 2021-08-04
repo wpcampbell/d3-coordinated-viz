dfa23cf4bd4499e3bb74e6c2f90a293d7726f9a4
wpcampbell <77030540+wpcampbell@users.noreply.github.com>
2021-08-04 12:29:37 -0500

    8-4-21 --- 12:30 PM

--- js/main.js
+++ js/main.js
@@ -7,80 +7,68 @@ window.onload = setMap();
 function setMap(){
 
     //map frame dimensions
-    var width = 960,
+    const width = 960,
         height = 460;
-    
+
     //create new svg container for the map
-    var map = d3.select("body")
+    const map = d3.select("body")
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
         .attr("height", height);
-    
+
     //create Albers equal area conic projection centered on France
-    var projection = d3.geoAlbers()
-        .center([-89,43])
-        .rotate([-2, 0, 0])
+    const projection = d3.geoAlbers()
+        .center([0, 44.5])
+        .rotate([89, 0, 0])
         .parallels([43, 62])
-        .scale(100)
+        .scale(4500)
         .translate([width / 2, height / 2]);
-    
-    var path = d3.geoPath()
-		.projection(projection);
+
+    const path = d3.geoPath()
+        .projection(projection);
 
 //use Promise.all to parallelize asynchronous data loading
-var promises = [];
-promises.push(d3.csv("data/vegdata.csv"));
+    const promises = [];
+    promises.push(d3.csv("data/vegdata.csv"));
 promises.push(d3.json("data/veg_area.topojson"))
-promises.push(d3.json("data/states.topojson"))
 Promise.all(promises).then(callback);
 
 //callback function    
 function callback(data){
     //create graticule generator
-var graticule = d3.geoGraticule()
-    .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+    const graticule = d3.geoGraticule()
+        .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
 //create graticule background
-var gratBackground = map.append("path")
-.datum(graticule.outline()) //bind graticule background
-.attr("class", "gratBackground") //assign class for styling
-.attr("d", path) //project graticule
+    const gratBackground = map.append("path")
+        .datum(graticule.outline()) //bind graticule background
+        .attr("class", "gratBackground") //assign class for styling
+        .attr("d", path); //project graticule
 
 //create graticule lines
-var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-.data(graticule.lines()) //bind graticule lines to each element to be created
-.enter() //create an element for each datum
-.append("path") //append each element to the svg as a path element
-.attr("class", "gratLines") //assign class for styling
-.attr("d", path); //project graticule lines
+    const gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+        .data(graticule.lines()) //bind graticule lines to each element to be created
+        .enter() //create an element for each datum
+        .append("path") //append each element to the svg as a path element
+        .attr("class", "gratLines") //assign class for styling
+        .attr("d", path); //project graticule lines
 
 
     vegdata_csv = data[0];
     vegdata_topojson = data[1];
-    states_topojson = data[2];
 
     console.log("This is the data array",data)//shows data array on console
 
     //translate vegetation topojson to geojson
-    var vegetationTopojson = topojson.feature(vegdata_topojson,vegdata_topojson.objects.veg_area),
-        statesTopojson = topojson.feature(states_topojson,states_topojson.objects.states);
+    const vegetationTopojson = topojson.feature(vegdata_topojson, vegdata_topojson.objects.veg_area);
 
     console.log("This is the converted veg geojson",vegetationTopojson)
-    console.log("This is the converted states geojson",statesTopojson)
-    // add the USA geojson to the map
-    var states = map.append("path")
-        .datum(statesTopojson)
-        .attr("class","states")
-        .attr("d", path);
 
     // add the Wisconsin geojson to the map
-    var wisconsin = map.append("path")
-        .datum(vegdata_topojson)
-        .attr("class","states")
-        .attr("d",path);
-
-    }; //end of function callback
+    const wisconsin = map.append("path")
+        .datum(vegetationTopojson)
+        .attr("class", "wisconsin")
+        .attr("d", path);
+}; //end of function callback
 };// end of function setMap
-
-
